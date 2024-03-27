35c0657d4a7fe39a9d8e2813a5dd9e444a6d9954
correalm <luismigueldasilva.99@gmail.com>
2024-03-27 13:35:53 -0300

    fix: remove old remaps

--- nvim/lua/correalm/remap.lua
+++ nvim/lua/correalm/remap.lua
@@ -1,7 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>ti", "<cmd>:IBLToggle<CR>")
-vim.keymap.set("n", "<leader>l", "<cmd>:TmuxNavigateLeft<CR>")
-vim.keymap.set("n", "<leader>ç", "<cmd>:TmuxNavigateRight<CR>")
 
 
