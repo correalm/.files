3a3df91bb0b30f2080dba6360c1160d319ee34ca
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-06-11 23:12:21 -0300

    increase lazygit windown size

--- nvim/after/plugin/floaterm.lua
+++ nvim/after/plugin/floaterm.lua
@@ -1,2 +1,6 @@
-vim.keymap.set("n", "<leader>tg", "<cmd>:FloatermNew lazygit<CR>")
+vim.keymap.set(
+  "n",
+  "<leader>tg",
+  "<cmd>::FloatermNew --height=0.98 --width=0.98 --wintype=float --name=lazygit --position=center lazygit<CR>"
+)
 vim.keymap.set("n", "<leader>t", "<cmd>:FloatermNew<CR>")
