0c4562614a7959ac4d2e27ddf732aea38e8e21cb
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-07-30 21:57:19 -0300

    remove lualine

--- nvim/lua/correalm/packer.lua
+++ nvim/lua/correalm/packer.lua
@@ -15,11 +15,6 @@ return require('packer').startup(function(use)
 
   use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'} })
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-
   use 'ThePrimeagen/harpoon'
 
   -- THEMES
