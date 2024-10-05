3e234c2b06c984b0567de071a5eebd54ce816ad0
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-10-05 09:24:28 -0300

    yellow line numbers are nice

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,6 +1,8 @@
 function ColorMyPencils(color)
   color = color or "poimandres"
-	vim.cmd.colorscheme(color)
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, 'LineNr', { fg='yellow' })
 
   -- vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
