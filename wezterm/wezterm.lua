06ed4af86169a01f1e133df5ea91c2cb0a9ea636
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-12-03 08:41:15 -0300

    hide tabs bar and adjust font size/line height on Wez

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,11 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 14.0
+config.font_size = 13.0
+config.line_height = 1.2
+config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
   left = 0,
   right = 0,
-  top = 2,
+  top = 0,
   bottom = 0,
 }
 
