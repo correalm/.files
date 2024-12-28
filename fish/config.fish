ee0a49b6c2a35f55907ea36b2bebeb3446206975
correalm <luismigueldasilva.99@gmail.com>
2024-12-28 18:02:05 -0300

    fish alias and plugins

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+[ -s "/home/correalm/.jabba/jabba.fish" ]; and source "/home/correalm/.jabba/jabba.fish"
+alias android='/usr/local/android-studio/bin/studio.sh'
